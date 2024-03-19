de83faa5ded916974cbfc6bf43771c5944450c29
Yee <yee.gamedev@gmail.com>
2024-03-19 11:05:49 -0600

    Lecture 13 - Bit Toggling and Unity Specifics

---
+++ Assets/HitRay.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitRay : MonoBehaviour
+{
+    void Update()
+    {
+        int layerMask = (1 << 7) | (1 << 6);
+
+        RaycastHit hit;
+
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),
+            out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
+            Debug.Log("Missed");
+        }
+    }
+}
