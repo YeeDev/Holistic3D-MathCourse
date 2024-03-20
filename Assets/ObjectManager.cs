dd0ad16531a275ea466f418616c0190fd08dffa6
Yee <yee.gamedev@gmail.com>
2024-03-20 12:32:55 -0600

    Lecture 23 - Practice Locating 2D Points on a Cartesian Plane

---
+++ Assets/ObjectManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ObjectManager : MonoBehaviour
+{
+    public GameObject objPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject obj = Instantiate(objPrefab,
+                                        new Vector3(Random.Range(-100, 100),
+                                                    Random.Range(-100, 100),
+                                                    objPrefab.transform.position.z),
+                                        Quaternion.identity);
+
+        Debug.Log($"Fuel location: {obj.transform.position}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
