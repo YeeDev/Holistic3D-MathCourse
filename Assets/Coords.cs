ba080bad0aea60b09f29f2d1f992f3a3b2db8ff9
Yee <yee.gamedev@gmail.com>
2024-03-20 10:54:03 -0600

    Lecture 21 - Drawing Lines: Solution

--- Assets/Coords.cs
+++ Assets/Coords.cs
@@ -33,15 +33,15 @@ public class Coords
         lineRenderer.endWidth = width;
     }
 
-    static public void DrawLine(Coords startPoint, Coords endPoint, float width, Color colour)
+    static public void DrawLine(Coords startPos, Coords endPos, float width, Color colour)
     {
-        GameObject line = new GameObject("Point_" + startPoint.ToString());
+        GameObject line = new GameObject($"Line_{startPos}_{endPos}");
         LineRenderer lineRenderer = line.AddComponent<LineRenderer>();
         lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
         lineRenderer.material.color = colour;
         lineRenderer.positionCount = 2;
-        lineRenderer.SetPosition(0, new Vector3(startPoint.x, startPoint.y, startPoint.z));
-        lineRenderer.SetPosition(1, new Vector3(endPoint.x, endPoint.y, endPoint.z));
+        lineRenderer.SetPosition(0, new Vector3(startPos.x, startPos.y, startPos.z));
+        lineRenderer.SetPosition(1, new Vector3(endPos.x, endPos.y, endPos.z));
         lineRenderer.startWidth = width;
         lineRenderer.endWidth = width;
     }
--- Assets/DrawLines.cs
+++ Assets/DrawLines.cs
@@ -4,19 +4,50 @@ using UnityEngine;
 
 public class DrawLines : MonoBehaviour
 {
-    Coords point = new Coords(10, 20);
+    //Coords point = new Coords(10, 20);
 
-    Coords startPoint = new Coords(0, 100);
-    Coords endPoint = new Coords(0, -100);
+    Coords startPointXAxis = new Coords(160, 0);
+    Coords endPointXAxis = new Coords(-160, 0);
+    Coords startPointYAxis = new Coords(0, 100);
+    Coords endPointYAxis = new Coords(0, -100);
+
+    Coords[] capricorn = {  new Coords(0, 15),
+                            new Coords(3, 18),
+                            new Coords(9, 22),
+                            new Coords(15, 23),
+                            new Coords(39, 35),
+                            new Coords(40, 30),
+                            new Coords(41, 25),
+                            new Coords(7, 12),
+                            new Coords(14, 9),
+                            new Coords(42, 9) };
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(point.ToString());
-        Coords.DrawPoint(new Coords(0, 0), 2, Color.red);
-        Coords.DrawPoint(point, 2, Color.green);
+        //Debug.Log(point.ToString());
+        //Coords.DrawPoint(point, 2, Color.green);
 
-        Coords.DrawLine(startPoint, endPoint, 1, Color.green);
+        //x axis
+        Coords.DrawLine(startPointXAxis, endPointXAxis, 1f, Color.red);
+        //y axis
+        Coords.DrawLine(startPointYAxis, endPointYAxis, 1f, Color.green);
+
+        foreach (Coords coord in capricorn)
+        {
+            Coords.DrawPoint(coord, 1, Color.cyan);
+        }
+
+        Coords.DrawLine(capricorn[0], capricorn[1], 0.4f, Color.yellow);
+        Coords.DrawLine(capricorn[1], capricorn[2], 0.4f, Color.yellow);
+        Coords.DrawLine(capricorn[2], capricorn[3], 0.4f, Color.yellow);
+        Coords.DrawLine(capricorn[3], capricorn[6], 0.4f, Color.yellow);
+        Coords.DrawLine(capricorn[4], capricorn[5], 0.4f, Color.yellow);
+        Coords.DrawLine(capricorn[5], capricorn[6], 0.4f, Color.yellow);
+        Coords.DrawLine(capricorn[6], capricorn[9], 0.4f, Color.yellow);
+        Coords.DrawLine(capricorn[0], capricorn[7], 0.4f, Color.yellow);
+        Coords.DrawLine(capricorn[7], capricorn[8], 0.4f, Color.yellow);
+        Coords.DrawLine(capricorn[8], capricorn[9], 0.4f, Color.yellow);
     }
 
     // Update is called once per frame
