a710223b3909b98e21470a4a7abb9969d7b6d55f
Yee <yee.gamedev@gmail.com>
2024-03-20 21:03:09 -0600

    Lecture 26 - Introduction to Vectors Part 1

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -1,42 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 
 // A very simplistic car driving on the x-z plane.
 
 public class Drive : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float rotationSpeed = 100.0f;
-    public Text energyAmount;
-    Vector3 currentLocation;
-
-    private void Start()
-    {
-        currentLocation = transform.position;
-    }
+    [SerializeField] float speed;
 
     void Update()
     {
-        if (float.Parse(energyAmount.text) <= 0) { return; }
 
-        // Get the horizontal and vertical axis.
-        // By default they are mapped to the arrow keys.
-        // The value is in the range -1 to 1
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        if (Input.GetKey(KeyCode.DownArrow)) { Move(new Vector2(0, -1)); }
+        else if (Input.GetKey(KeyCode.UpArrow)) { Move(new Vector2(0, 1)); }
+        if (Input.GetKey(KeyCode.LeftArrow)) { Move(new Vector2(-1, 0)); }
+        else if (Input.GetKey(KeyCode.RightArrow)) { Move(new Vector2(1, 0)); }
 
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
 
-        // Move translation along the object's z-axis
-        transform.Translate(0, translation, 0);
+    }
 
-        // Rotate around our y-axis
-        transform.Rotate(0, 0, -rotation);
+    private void Move(Vector2 direction)
+    {
+        Vector3 position = this.transform.position;
+        position.x += direction.x * speed;
+        position.y += direction.y *  speed;
 
-        energyAmount.text = (float.Parse(energyAmount.text) - Vector3.Distance(currentLocation, transform.position)) + "";
-        currentLocation = this.transform.position;
+        this.transform.position = position;
     }
 }
