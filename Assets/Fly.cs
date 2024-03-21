c37816e38b37b123ae02441e57ef07b118060287
Yee <yee.gamedev@gmail.com>
2024-03-21 17:14:34 -0600

    Lecture 39 - Moving from 2D to 3D

---
+++ Assets/Fly.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : MonoBehaviour
+{
+    public float rotationSpeed = 1.0f;
+    public float speed = 1.0f;
+
+    void Update()
+    {
+        float translateX = Input.GetAxis("Horizontal") * speed;
+        float translateY = Input.GetAxis("VerticalY") * speed;
+        float translateZ = Input.GetAxis("Vertical") * speed;
+
+        transform.Translate(translateX, 0, 0);
+        transform.Translate(0, translateY, 0);
+        transform.Translate(0, 0, translateZ);
+    }
+}
