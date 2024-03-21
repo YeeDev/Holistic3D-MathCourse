b19c9c39697a73f2e06ca2e8c8ef192a1dfd5cf1
Yee <yee.gamedev@gmail.com>
2024-03-20 21:38:19 -0600

    Lecture 27 - Introduction to Vectors Part 2

--- Assets/Scripts/Drive.cs
+++ Assets/Scripts/Drive.cs
@@ -5,25 +5,57 @@ using System.Collections;
 
 public class Drive : MonoBehaviour
 {
-    [SerializeField] float speed;
+    Vector2 Up = new Vector2(0, 1);
+    Vector2 Right = new Vector2(1, 0);
+    float speed = 0.2f;
 
     void Update()
     {
-
-        if (Input.GetKey(KeyCode.DownArrow)) { Move(new Vector2(0, -1)); }
-        else if (Input.GetKey(KeyCode.UpArrow)) { Move(new Vector2(0, 1)); }
-        if (Input.GetKey(KeyCode.LeftArrow)) { Move(new Vector2(-1, 0)); }
-        else if (Input.GetKey(KeyCode.RightArrow)) { Move(new Vector2(1, 0)); }
-
-
-    }
-
-    private void Move(Vector2 direction)
-    {
         Vector3 position = this.transform.position;
-        position.x += direction.x * speed;
-        position.y += direction.y *  speed;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            position.x += Up.x * speed;
+            position.y += Up.y * speed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            position.x += -Up.x * speed;
+            position.y += -Up.y * speed;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            position.x += -Right.x * speed;
+            position.y += -Right.y * speed;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            position.x += Right.x * speed;
+            position.y += Right.y * speed;
+        }
 
-        this.transform.position = position;
+        transform.position = position;
     }
+
+    #region My Solution
+
+    //[SerializeField] float speed;
+
+    //void Update()
+    //{
+
+    //    if (Input.GetKey(KeyCode.DownArrow)) { Move(new Vector2(0, -1)); }
+    //    else if (Input.GetKey(KeyCode.UpArrow)) { Move(new Vector2(0, 1)); }
+    //    if (Input.GetKey(KeyCode.LeftArrow)) { Move(new Vector2(-1, 0)); }
+    //    else if (Input.GetKey(KeyCode.RightArrow)) { Move(new Vector2(1, 0)); }
+    //}
+
+    //private void Move(Vector2 direction)
+    //{
+    //    Vector3 position = this.transform.position;
+    //    position.x += direction.x * speed;
+    //    position.y += direction.y *  speed;
+
+    //    this.transform.position = position;
+    //}
+    #endregion
 }
