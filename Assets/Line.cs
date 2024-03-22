17a2be59fcc9aa2be22e5d951a3b167ae69484fa
Yee <yee.gamedev@gmail.com>
2024-03-21 23:02:53 -0600

    Lecture 41 - Lines, Line Segments and Rays Part 1

---
+++ Assets/Line.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    Coords A;
+    Coords B;
+    Coords v;
+
+    public Line(Coords A, Coords B)
+    {
+        this.A = A;
+        this.B = B;
+        v = new Coords(B.x - A.x, B.y - A.y, B.z - A.z);
+    }
+
+    public Coords GetPointAt(float t)
+    {
+        float xt = A.x + v.x * t;
+        float yt = A.y + v.y * t;
+        float zt = A.z + v.z * t;
+
+        return new Coords(xt, yt, zt);
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+    Line line;
+
+    void Start()
+    {
+        line = new Line(new Coords(start.position), new Coords(end.position));
+    }
+
+    private void Update()
+    {
+        transform.position = line.GetPointAt(Time.time).ToVector();
+    }
+}
