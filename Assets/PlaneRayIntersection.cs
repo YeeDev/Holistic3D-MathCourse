fe19998562df6427e39c275a6ebabb1fbe1b567b
Yee <yee.gamedev@gmail.com>
2024-03-27 13:20:49 -0600

    Lecture 52 - More Unity Specifics Part 1

--- Assets/PlaneRayIntersection.cs
+++ Assets/PlaneRayIntersection.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class PlaneRayIntersection : MonoBehaviour
 {
-    public GameObject sphere;
-    //public Transform corner1;
-    //public Transform corner2;
-    //public Transform corner3;
+    public GameObject sheep;
     public GameObject quad;
+    public GameObject[] fences;
 
     Plane mPlane;
 
@@ -16,13 +14,9 @@ public class PlaneRayIntersection : MonoBehaviour
     void Start()
     {
         Vector3[] vertices = quad.GetComponent<MeshFilter>().mesh.vertices;
-        mPlane = new Plane(quad.transform.TransformPoint(vertices[0]),
-                            quad.transform.TransformPoint(vertices[1]),
-                            quad.transform.TransformPoint(vertices[2]));
-
-        Debug.Log(vertices[0]);
-        Debug.Log(vertices[1]);
-        Debug.Log(vertices[2]);
+        mPlane = new Plane(quad.transform.TransformPoint(vertices[0]) + new Vector3(0, 0.3f, 0),
+                            quad.transform.TransformPoint(vertices[1]) + new Vector3(0, 0.3f, 0),
+                            quad.transform.TransformPoint(vertices[2]) + new Vector3(0, 0.3f, 0));
     }
 
     // Update is called once per frame
@@ -36,8 +30,37 @@ public class PlaneRayIntersection : MonoBehaviour
             if (mPlane.Raycast(ray, out t))
             {
                 Vector3 hitPoint = ray.GetPoint(t);
-                sphere.transform.position = hitPoint;
+
+                GameObject fence = GetClosestFence(hitPoint);
+                Vector3[] vertices = fence.GetComponent<MeshFilter>().sharedMesh.vertices;
+                Vector3 u = fence.transform.TransformPoint(vertices[1]) - fence.transform.TransformPoint(vertices[0]);
+                Vector3 v = fence.transform.TransformPoint(vertices[2]) - fence.transform.TransformPoint(vertices[0]);
+                Vector3 normal = Vector3.Cross(u, v);
+                Vector3 c = fence.transform.position - hitPoint;
+
+                Debug.DrawLine(fence.transform.position, normal, Color.red);
+                Debug.DrawRay(hitPoint, c);
+                Debug.Log(Vector3.Dot(normal, c));
+                if(Vector3.Dot(normal, c) >= 0)
+                    sheep.transform.position = hitPoint;
             }
         }
+    }
+
+    GameObject GetClosestFence(Vector3 hitPoint)
+    {
+        GameObject closestFence = null;
+        float distance = Mathf.Infinity;
+        foreach (GameObject fence in fences)
+        {
+            float distanceToCheck = Vector3.Distance(hitPoint, fence.transform.position);
+            if (distanceToCheck < distance)
+            {
+                distance = distanceToCheck;
+                closestFence = fence;
+            }
+        }
+
+        return closestFence;
     }
 }
